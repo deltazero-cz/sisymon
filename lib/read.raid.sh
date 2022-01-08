8c6259db9de3e3481610154edf805ae19e758ecc
ΔO <david@deltazero.cz>
2022-01-08 18:38:07 +0100

    check raid health - mdstat

--- lib/read.raid.sh
+++ lib/read.raid.sh
@@ -5,10 +5,14 @@ read.raid() {
 
   json='[]'
   while (("$#")); do
+    array=$(grep "$1" /proc/mdstat | cut -f 3- -d " ")
+    [[ -n "$array" ]] && array="\"$array\""
+
     status=$(echo "$mdstat" | grep "$1" | awk '{print $2}')
     [[ -n "$status" ]] && status="\"$status\""
 
     item=$(jq ".name = \"/dev/${1}\"
+             | .array = ${array:-null}
              | .status = ${status:-null}
            " -c <<< '{}')
 
